91e1553c26cc1897c69c8dc2490e3fee468c2469
Shrief Essam <shriefessam1999@gmail.com>
2022-08-05 15:23:37 +0200

    initialize the project

---
+++ main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const button = document.querySelector('button');
+button.addEventListener('click',()=>{
+    console.log('sdsdsd');
+})
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYXRoZXJhcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgYnV0dG9uID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignYnV0dG9uJyk7XG5idXR0b24uYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCgpPT57XG4gICAgY29uc29sZS5sb2coJ3Nkc2RzZCcpO1xufSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
