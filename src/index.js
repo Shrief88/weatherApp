0ad6bf9fe94ece355e723214e81ebe65f52da92d
Shrief Essam <shriefessam1999@gmail.com>
2022-08-06 16:32:08 +0200

    add the input element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  console.log('sdsdsd');
-});
+import './style.css';
+
+
+const getWeatherData = async (city)=>{
+    const key = 'fad27574298abfd989e6954830b1e46c';
+    const respose = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`,{mode:'cors'});
+    const weatherData = await respose.json();
+    console.log(weatherData);
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,20 @@ module.exports = {
       },
       {
         test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env',{ targets: "defaults" }]
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
           }
         }
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
